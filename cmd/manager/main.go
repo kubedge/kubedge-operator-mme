56f5504711a677cc92960858ec42a39c2e792e4c
Jerome Brette <jbrette@gmail.com>
2019-04-06 17:25:32 +0000

    Update lock name

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -60,7 +60,7 @@ func main() {
 	ctx := context.TODO()
 
 	// Become the leader before proceeding
-	err = leader.Become(ctx, "kubedgeoperator-operator-lock")
+	err = leader.Become(ctx, "kubedge-mme-operator-lock")
 	if err != nil {
 		log.Error(err, "")
 		os.Exit(1)
